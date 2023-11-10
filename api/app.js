d0833b0a8c0670377cb495421b40819d13513354
franco <francoronaldocarlos@gmail.com>
2023-11-09 23:55:46 -0300

    hola mundo

---
+++ api/app.js
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import express from "express";
+
+import { comprasRouter } from "./compras.js";
+import { ventasRouter } from "./ventas.js";
+
+// Creo aplicacion express
+const app = express();
+app.use(express.json());
+
+app.use("/compras", comprasRouter);
+app.use("/ventas", ventasRouter);
+
+// Registrar metodo GET en ruta raiz ('/')
+app.get("/", (req, res) => {
+  res.send("Hola mundo");
+});
+// Pongo en funcionamiento la API en puerto 3000
+app.listen(3000, () => {
+  console.log("API en funcionamiento");
+});
+
