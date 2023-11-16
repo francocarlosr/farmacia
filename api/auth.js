9bbf6b6ca3b997df692fe4aa92c81f630360eac3
francocarlosr <94251503+francocarlosr@users.noreply.github.com>
2023-11-16 16:26:04 -0300

    Create auth.js
    auth terminado

---
+++ api/auth.js
@@ -0,0 +1,97 @@
+import passport from "passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { body, validationResult } from "express-validator";
+import { db } from "./db.js";
+
+export function authConfig() {
+  const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET,
+  };
+
+  passport.use(
+    new Strategy(jwtOptions, async (payload, next) => {
+      const [rows, fields] = await db.execute(
+        "SELECT usuario FROM empleado WHERE usuario = :usuario",
+        { usuario: payload.usuario }
+      );
+      if (rows.length > 0) {
+        next(null, rows[0]);
+      } else {
+        next(null, false);
+      }
+    })
+  );
+}
+
+export const authRouter = express
+  .Router()
+
+  .post(
+    "/login",
+    body("usuario").isAlphanumeric().isLength({ min: 1, max: 25 }),
+    body("password").isStrongPassword({
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 0,
+    }),
+    async (req, res) => {
+      const validacion = validationResult(req);
+      if (!validacion.isEmpty()) {
+        res.status(400).send({ errors: validacion.array() });
+        return;
+      }
+
+      const { usuario, password } = req.body;
+
+      // Obtengo cuenta de usuario
+      const [rows, fields] = await db.execute(
+        `SELECT
+           e.usuario,
+           e.password
+         FROM empleados e
+         WHERE usuario = :usuario`,
+        { usuario }
+      );
+      if (rows.length === 0) {
+        res.status(400).send("Usuario o contraseña inválida");
+        return;
+      }
+
+      const user = rows[0];
+
+      // Verificar contraseña
+      const passwordCompared = await bcrypt.compare(password, user.password);
+      if (!passwordCompared) {
+        res.status(400).send("Usuario o contraseña inválida");
+        return;
+      }
+
+      // Generar token
+      const payload = { usuario };
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "2h",
+      });
+
+      // Sesion en WEB
+      const sesion = {
+        usuario: user.usuario,
+        token,
+      };
+
+      res.send(sesion);
+    }
+  )
+
+  .get(
+    "/perfil",
+    passport.authenticate("jwt", { session: true }),
+    (req, res) => {
+      res.json(req.user);
+    }
+  );
