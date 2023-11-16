c0c3510780690ecddff4faf510dda2378a6c3b15
francocarlosr <94251503+francocarlosr@users.noreply.github.com>
2023-11-16 01:12:22 -0300

    Update compras.js
    RUTA COMPRAS TERMINADA

--- api/compras.js
+++ api/compras.js
@@ -14,6 +14,30 @@ export const comprasRouter = express
   .get("/:id", async (req, res) => {
     const { id } = req.params;
     const [rows, fields] = await db.execute(
+      "SELECT * FROM compra WHERE id = :id",
+      { id }
+    );
+    if (rows.length > 0) {
+      res.send(rows[0]);
+    } else {
+      res.status(404).send({ mensaje: "compra no encontrada" });
+    }
+  })
+
+    // compra por id
+  .delete("/:id", async (req, res) => {
+      const { id } = req.params;
+      const [rows, fields] = await db.execute(
+        "DELETE FROM compra WHERE id = :id",
+        { id }
+      );
+      res.send("ok");
+    })
+
+  // detalle por id
+  .get("/:id/detallecompra", async (req, res) => {
+    const { id } = req.params;
+    const [rows, fields] = await db.execute(
       "SELECT dc.id, p.nombre AS producto, dc.cantidad, dc.precio  FROM detallecompra dc \
       JOIN producto p ON dc.producto_id = p.id \
       WHERE dc.compra_id = :id",
@@ -22,11 +46,11 @@ export const comprasRouter = express
     if (rows.length > 0) {
       res.send(rows);
     } else {
-      res.status(404).send({ mensaje: "compra no encontrada" });
+      res.status(404).send({ mensaje: "detalles no encontrados" });
     }
   })
 
-  
+// Agregar nueva compra
   .post("/", async (req, res) => {
     const nuevaCompra = req.body.nuevaCompra;
     const [rows] = await db.execute(
@@ -39,18 +63,21 @@ export const comprasRouter = express
     res.status(201).send({ mensaje: "Compra Creada" });
   })
 
-
-
+// Agregar nuevo detalle de compra
   .post("/detallecompra", async (req, res) => {
-    const nuevoDetallecompra = req.body.turno;
+    const nuevoDetalle = req.body.turno;
     const [rows] = await db.execute(
       "insert into detallecompra (cantidad, precio, producto_id, compra_id) values (:cantidad, :precio, :producto_id, :compra_id)",
       {
-        fecha: nuevoDetallecompra.fecha,
-        precio: nuevoDetallecompra.precio,
-        producto_id:nuevoDetallecompra.producto_id,
-        compra_id:nuevoDetallecompra.compra_id,
-      }
+        cantidad: nuevoDetalle.cantidad,
+        precio: nuevoDetalle.precio,
+        producto_id:nuevoDetalle.producto_id,
+        compra_id:nuevoDetalle.compra_id
+      },
+      "UPDATE productos SET stock=stock+cantidad WHERE id=id_producto"
     );
     res.status(201).send({ mensaje: "Detalle Creado" });
   })
+
+
+  
